9fc63a71a3e639cc016d77019bb9b88f897b9567
Semion Mudrov <mudrsemen@ya.ru>
2025-02-10 19:24:30 +0300

    Feat: added menu & modal action, added smoth scroll

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import timer from './modules/timer';
+import menu from './modules/menu';
+import modal from './modules/modal';
+import smoothScroll from './modules/smoothScroll';
 
 timer('15 february 2025 18:12');
+menu();
+modal();
+smoothScroll();
