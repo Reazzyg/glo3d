d645a4676ac708f88e863f4464aec0715ae07c3b
Semion Mudrov <mudrsemen@ya.ru>
2025-02-24 23:34:57 +0300

    Fix: fixed incorrect menu closing

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,8 +1,5 @@
 export default function menu() {
-  const menuBtn = document.querySelector('.menu');
   const menu = document.querySelector('menu');
-  const closeBtn = menu.querySelector('.close-btn');
-  const menuItems = menu.querySelectorAll('ul>li>a');
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
@@ -10,7 +7,7 @@ export default function menu() {
 
   document.body.addEventListener('click', (e) => {
     if (
-      e.target.closest('menu') ||
+      e.target.matches('.active-menu ul li a') ||
       e.target.matches('.close-btn') ||
       e.target.closest('.menu') ||
       (menu.classList.contains('active-menu') && !e.target.closest('menu'))
